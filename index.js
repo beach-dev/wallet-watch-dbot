6c1f63a8e08a04c0483016c9f175befb57506901
liu8321 <yangliu8321@gmail.com>
2021-05-13 17:45:34 +0900

    test1

--- index.js
+++ index.js
@@ -1,67 +1,79 @@
 require('dotenv').config()
 const fetch = require('node-fetch')
+const BlocknativeSdk = require('bnc-sdk');
+const WebSocket = require('ws');
 
-var intervalId = null;
+// blocknative initialize
 
-const Discord = require('discord.js');
-const client = new Discord.Client({
-    partials: ['MESSAGE ']
-});
-const interval = process.env.INTERVAL || 60;
+const options = {
+    dappId: process.env.DAPP_ID,
+    networkId: 56,
+    system: 'ethereum', // optional, defaults to ethereum
+    transactionHandlers: [event => console.log(event.transaction)],
+    ws: WebSocket, // only neccessary in server environments
+    name: 'Wallet watcher' // optional, use when running multiple instances
+};
 
-client.on("ready", () => {
-    console.log("Bot is ready")
+const blocknative = new BlocknativeSdk(options)
+
+var address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+
+const {
+    emitter, // emitter object to listen for status updates
+    details // initial account details which are useful for internal tracking: address
+  } = blocknative.account(address);
+
+console.log(details);
+
+emitter.on('txPool', transaction => {
+console.log(`Sending ${transaction.value} wei to ${transaction.to}`)
 })
 
-const checkRoles = () => {
+emitter.on('txConfirmed', transaction => {
+console.log('Transaction is confirmed!')
+})
+
+// catch every other event that occurs and log it
+emitter.on('all', transaction => {
+console.log(`Transaction event: ${transaction.eventCode}`)
+})
+
+// var wallet = null;
+
+// const Discord = require('discord.js');
+// const client = new Discord.Client({
+//     partials: ['MESSAGE ']
+// });
+
+// client.on("ready", () => {
+//     console.log("Watch Bot is ready")
+// })
+
+// const startWatch = () => {
+
+// }
+
+// const stopWatch = () => {
     
-    console.log('!!! check role');
-    client.guilds.fetch(process.env.GUILDID, true, true).then(
-        list =>
-            list.members.fetch()
-            .then( members => 
-                members.each(member => {
-                    
-                    fetch(`http://3.142.255.213/api/articles/${member.id}`)
-                    .then(res => res.json())
-                    .then(data => {
-                        if(data.punkbodies > 0) {
-                            member.roles.add(process.env.PUNKBODIES_ROLE_ID);
-                        } else {
-                            member.roles.remove(process.env.PUNKBODIES_ROLE_ID)
-                        }
-                        if(data.punkster > 0) {
-                            member.roles.add(process.env.PUNKSTER_ROLE_ID);
-                        } else {
-                            member.roles.remove(process.env.PUNKSTER_ROLE_ID)
-                        }
-                    })
-                    .catch(console.error)
-                })
-            )
-    );
-}
+// }
 
-client.on("message", msg => {
-    if (msg.content === "$$$test$$$") {
+// client.on("message", msg => {
+//     if (msg.content === "$$$test$$$") {
 
-        // msg.delete();
-        checkRoles();
-    } 
-    else if (msg.content === "$$$start$$$") { 
+//         msg.reply("I'm here!");
+//     } 
+//     else if (msg.content.startsWith("$$$wallet")) { 
 
-        // msg.delete();
-        if (!intervalId) {
-            intervalId = setInterval (checkRoles, interval * 1000); 
-        }
-    } 
-    else if (msg.content === "$$$stop$$$") { 
+//         console.log("!!! start watch");
+
+//         wallet = msg.content.substring(10);
+//         startWatch();
+//     } 
+//     else if (msg.content === "$$$stop$$$") { 
         
-        // msg.delete();
-        if (intervalId)
-            clearInterval(intervalId);
-            intervalId = null;
-      }
-})
+//         console.log("!!! stop watch");
+//         stopWatch();
+//       }
+// })
 
-client.login(process.env.BOT_TOKEN)
+// client.login(process.env.BOT_TOKEN)
