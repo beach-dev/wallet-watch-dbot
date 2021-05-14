f88a7236027a5ccecbd3b0360cb4c5b891eae4a3
liu8321 <yangliu8321@gmail.com>
2021-05-14 23:32:33 +0900

    working 2

--- index.js
+++ index.js
@@ -11,69 +11,65 @@ const options = {
     system: 'ethereum', // optional, defaults to ethereum
     transactionHandlers: [event => console.log(event.transaction)],
     ws: WebSocket, // only neccessary in server environments
-    name: 'Wallet watcher' // optional, use when running multiple instances
+    name: 'Mempool Explorer' // optional, use when running multiple instances
 };
 
 const blocknative = new BlocknativeSdk(options)
 
-var address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 
-const {
-    emitter, // emitter object to listen for status updates
-    details // initial account details which are useful for internal tracking: address
-  } = blocknative.account(address);
+const Discord = require('discord.js');
+const client = new Discord.Client({
+    partials: ['MESSAGE ']
+});
 
-console.log(details);
+const startWatch = async (address) => {
 
-emitter.on('txPool', transaction => {
-console.log(`Sending ${transaction.value} wei to ${transaction.to}`)
-})
-
-emitter.on('txConfirmed', transaction => {
-console.log('Transaction is confirmed!')
-})
-
-// catch every other event that occurs and log it
-emitter.on('all', transaction => {
-console.log(`Transaction event: ${transaction.eventCode}`)
-})
-
-// var wallet = null;
-
-// const Discord = require('discord.js');
-// const client = new Discord.Client({
-//     partials: ['MESSAGE ']
-// });
-
-// client.on("ready", () => {
-//     console.log("Watch Bot is ready")
-// })
-
-// const startWatch = () => {
-
-// }
-
-// const stopWatch = () => {
+    var config = {
+        scope: address,
+        watchAddress: true
+      };
     
-// }
+    const {
+        emitter, // emitter object to listen for status updates
+        details // initial account details which are useful for internal tracking: address
+      } = await blocknative.configuration(config);
+    
+    
+    // catch every other event that occurs and log it
+    emitter.on('all', transaction => {
+        console.log(`Transaction event: ${transaction.eventCode}`)
+    })
+}
 
-// client.on("message", msg => {
-//     if (msg.content === "$$$test$$$") {
+const stopWatch = () => {
+    
+}
 
-//         msg.reply("I'm here!");
-//     } 
-//     else if (msg.content.startsWith("$$$wallet")) { 
 
-//         console.log("!!! start watch");
+client.on("ready", () => {
+    console.log("Watch Bot is ready")
+})
 
-//         wallet = msg.content.substring(10);
-//         startWatch();
-//     } 
-//     else if (msg.content === "$$$stop$$$") { 
+client.on("message", msg => {
+    if (msg.content === "$$$test$$$") {
+
+        msg.reply("I'm here!");
+    } 
+    else if (msg.content.startsWith("$$$wallet")) { 
+
+        console.log("!!! start watch");
+
+        wallet = msg.content.substring(10);
+        startWatch();
+    } 
+    else if (msg.content === "$$$stop$$$") { 
         
-//         console.log("!!! stop watch");
-//         stopWatch();
-//       }
-// })
+        console.log("!!! stop watch");
+        stopWatch();
+      }
+})
 
-// client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN)
+
+
+// startWatch('0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F')
