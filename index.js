571fa83ca0715128f230777d625bafd2573f4160
liu8321 <yangliu8321@gmail.com>
2021-05-15 01:32:04 +0900

    v1 completed

--- index.js
+++ index.js
@@ -5,59 +5,130 @@ const WebSocket = require('ws');
 
 // blocknative initialize
 
+var addresses = [];
+var labels = [];
+
 const options = {
     dappId: process.env.DAPP_ID,
     networkId: 56,
     system: 'ethereum', // optional, defaults to ethereum
-    transactionHandlers: [event => console.log(event.transaction)],
     ws: WebSocket, // only neccessary in server environments
     name: 'Mempool Explorer' // optional, use when running multiple instances
 };
 
 const blocknative = new BlocknativeSdk(options)
 
+var lastlog = '';
+var lasttime = new Date().getTime();
 
 const Discord = require('discord.js');
 const client = new Discord.Client({
     partials: ['MESSAGE ']
 });
 
-const startWatch = (address) => {
+const startWatch = (address, label, channel) => {
+
+    if (label && label.length > 0 && labels.indexOf(label) >= 0) {
+        channel.send('Duplicated label!');
+        return;
+    }
+
+    if (addresses.indexOf(address) >= 0) {
+        channel.send('Duplicated address!');
+        return;
+    }
+    
+    var index = addresses.push(address) - 1;
+    labels[index] = label;
 
     const { emitter } = blocknative.account(address)
     
     emitter.on('all', transaction => {
-        console.log(`Transaction event: ${transaction.eventCode}`)
+
+        var log = `Transaction event: ${transaction.eventCode}`;
+        console.log(log);
+
+        lastlog += log + '\n';
+
+        var currentTime = new Date().getTime();
+        if (currentTime - lasttime > 2000) {
+            channel.send(lastlog);
+            lastlog = '';
+            lasttime = currentTime;
+        }
     })
 }
 
-const stopWatch = () => {
+const stopWatchByAddress = (address, channel) => {
+
+    if (!address || address.length == 0) {
+        channel.send('Address is not valid!');
+        return;
+    }
+    var index = addresses.indexOf(address);
+
+    if (index < 0) {
+        channel.send('No registered address found!');
+        return;
+    }
+
+    var address = addresses[index];
     
+    blocknative.unsubscribe(address)
+    labels.splice(index, 1);
+    addresses.splice(index, 1);
 }
 
+const stopWatchByLabel = (label, channel) => {
+
+    if (!label || label.length == 0) {
+        channel.send('Label is not valid!');
+        return;
+    }
+    var index = labels.indexOf(label);
+
+    if (index < 0) {
+        channel.send('No registered label found!');
+        return;
+    }
+
+    var address = addresses[index];
+    
+    blocknative.unsubscribe(address)
+    labels.splice(index, 1);
+    addresses.splice(index, 1);
+}
 
 client.on("ready", () => {
     console.log("Watch Bot is ready")
 })
 
 client.on("message", msg => {
-    if (msg.content === "$$$test$$$") {
+    if (msg.content === "!watch-test") {
 
         msg.reply("I'm here!");
     } 
-    else if (msg.content.startsWith("$$$wallet")) { 
+    else if (msg.content.startsWith("!watch ")) { 
 
         console.log("!!! start watch");
 
-        wallet = msg.content.substring(10);
-        msg.channel.send(wallet);
-        startWatch();
+        parts = msg.content.split(' ');
+        startWatch(parts[1], parts[2], msg.channel);
     } 
-    else if (msg.content === "$$$stop$$$") { 
+    else if (msg.content.startsWith("!stopl ")) { 
         
-        console.log("!!! stop watch");
-        stopWatch();
-      }
+        console.log("!!! stop watch by label");
+        
+        parts = msg.content.split(' ');
+        stopWatchByLabel(parts[1], msg.channel);
+    } 
+    else if (msg.content.startsWith("!stopa ")) { 
+        
+        console.log("!!! stop watch by address");
+        
+        parts = msg.content.split(' ');
+        stopWatchByAddress(parts[1], msg.channel);
+    }
 })
 
 client.login(process.env.BOT_TOKEN)
